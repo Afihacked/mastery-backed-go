f514d56410c9e8fb7fbd2dcf4f370c846d53d2ac
Afitech <59785481+Afihacked@users.noreply.github.com>
2025-09-24 12:33:44 +0700

    Update yt_helpers.go

--- internal/utils/yt_helpers.go
+++ internal/utils/yt_helpers.go
@@ -45,7 +45,7 @@ func ExtractInfo(url string, format string, cookies string) (map[string]interfac
 
 	args = append(args, url)
 
-	cmd := exec.Command("yt-dlp", "-j", url)
+	cmd := exec.Command("/usr/local/bin/yt-dlp", args...)
 	out, err := cmd.Output()
 	if err != nil {
 		return map[string]interface{}{"success": false, "error": err.Error()}, err
